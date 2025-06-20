dffd8211ec049c2ba4cf1e8c2cd4b3bec5ef8364
chLi <565353780@qq.com>
2025-06-20 22:26:02 +0800

    finish a faster version of iso remeshing

--- iso_remesh/Demo/remesher.py
+++ iso_remesh/Demo/remesher.py
@@ -1,21 +1,13 @@
-import open3d as o3d
-
 from iso_remesh.Module.remesher import Remesher
 
 
 def demo():
     mesh_file_path = "/Users/chli/chLi/Dataset/Objaverse_82K/trimesh/000-000/000a00944e294f7a94f95d420fdd45eb.obj"
+    iso_mesh_file_path = "/Users/chli/chLi/Dataset/Objaverse_82K/iso_mesh/000-000/000a00944e294f7a94f95d420fdd45eb.obj"
 
-    mesh = o3d.io.read_triangle_mesh(mesh_file_path)
+    mesh_file_path = "/Users/chli/chLi/Dataset/vae-eval/mesh/002.obj"
+    iso_mesh_file_path = "/Users/chli/chLi/Dataset/vae-eval/iso_mesh/002.obj"
+    overwrite = True
 
-    remesher = Remesher(mesh)
-
-    remesher.isotropic_remeshing(
-        0.14,
-        iter=1,
-        explicit=False,
-        foldover=10,
-        sliver=True,
-    )
-
+    Remesher.isoRemesh(mesh_file_path, iso_mesh_file_path, overwrite)
     return True
